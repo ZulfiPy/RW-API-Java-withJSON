0d1acaa0d8e54ddd435e9ece58d5eecc793e6b26
Zu <eesti_zulf@Zus-Mac-mini.localdomain>
2025-05-03 19:28:13 +0300

    midagi pole veel valmis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
+
+    private static final String FILE_PATH = "/Users/eesti_zulf/IdeaProjects/RW_API_Java_withJSON/src/vehicles.json";
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        try {
+            var test = Files.readAllLines(Paths.get(FILE_PATH));
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't read the file", e);
+        }
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,8 @@
+public class Vehicle {
+    public String vin;
+    public String reg_plate;
+    public String make;
+    public String model;
+    public int year;
+    public String fuel_type;
+}

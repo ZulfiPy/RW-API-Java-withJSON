80c464cde6e1ae3dbd307e5457985ea6793f8543
Zulfugar A <zulfugara@Zulfugars-MacBook-Pro.local>
2025-05-04 19:39:41 +0300

    deliver: vehicles related functions delivered

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,74 @@ import java.io.IOException;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
 
-    private static final String FILE_PATH = "/Users/eesti_zulf/IdeaProjects/RW_API_Java_withJSON/src/vehicles.json";
+    private static final ObjectMapper mapper = new ObjectMapper();
+    public static List<Vehicle> vehicles;
 
     public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
+        Dotenv dotenv = Dotenv.load();
+
+//        String macminiM4Path = dotenv.get("MACMINI_M4_PATH");
+        String macbookM4Path = dotenv.get("MACBOOK_M4_PATH");
+
+        vehicles = getVehicles(macbookM4Path);
+
+//        Vehicle sampleVehicle = new Vehicle("WP1ZZZ92ZGLA73050", "759PFY", "Porsche", "Cayenne GTS", 2015, "Petrol", "Automatic", "Red", "Universal", "four wheel drive");
+//
+//        addVehicle(sampleVehicle);
+//
+//        writeJson(macbookM4Path, vehicles);
+    }
+
+    public static List<Vehicle> getVehicles(String filePath) {
+        File file = new File(filePath);
+        if (!file.exists() || !file.canRead()) {
+            throw new RuntimeException("the json file is not found or isn't readable");
+        }
 
         try {
-            List<Vehicle> vehicles = mapper.readValue(
-                    new File(FILE_PATH),
+            return mapper.readValue(
+                    new File(filePath),
                     new TypeReference<List<Vehicle>>() {}
             );
         } catch (IOException e) {
-            throw new RuntimeException("Couldn't read or parse the file", e);
+            throw new RuntimeException("Couldn't read or parse the json file", e);
+        }
+    }
+
+    public static boolean isDuplicatedVehicle(String vin, String reg_plate) {
+        if (!vehicles.isEmpty()) {
+            for (Vehicle vehicle : vehicles) {
+                if (vehicle.vin.equals(vin) || vehicle.reg_plate.equals(reg_plate)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public static boolean addVehicle(Vehicle newVehicle) {
+        if (isDuplicatedVehicle(newVehicle.vin, newVehicle.reg_plate)) {
+            return false;
+        }
+
+        vehicles.add(newVehicle);
+
+        return true;
+    }
+
+    public static <T> void writeJson(String filePath, T data) {
+        try {
+            File vehicleFile = new File(filePath);
+
+            mapper.writeValue(vehicleFile, data);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't write the json file", e);
         }
     }
 }

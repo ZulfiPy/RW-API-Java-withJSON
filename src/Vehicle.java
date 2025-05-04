e652d779e23b60b5aa93933a684010f422ccf09f
Zulfugar A <zulfugara@Zulfugars-MacBook-Pro.local>
2025-05-04 19:39:18 +0300

    deliver Vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -9,4 +9,26 @@ public class Vehicle {
     public String color;
     public String body;
     public String drivetrain;
+
+    public Vehicle() {
+
+    }
+
+    public Vehicle(String vin, String reg_plate, String make, String model, int year, String fuel_type, String transmission, String color, String body, String drivetrain) {
+        this.vin = vin;
+        this.reg_plate = reg_plate;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.fuel_type = fuel_type;
+        this.transmission = transmission;
+        this.color = color;
+        this.body = body;
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle: " + make + " " + model + " " + reg_plate;
+    }
 }
